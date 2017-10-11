53e420d80cfbaec791c6db4cb1e582ae42241b2b
fridde <f@hehl.se>
2017-10-11 19:04:47 +0200

    small mistake

--- src/SavedClient.php
+++ src/SavedClient.php
@@ -9,14 +9,13 @@ class SavedClient {
 	private $guzzle;
 	private $timeout = 2.0;
 	private $keys;
-	private $keyfile = 'keys.ini';
 	private $api_url = 'http://devapi.saved.io/bookmarks/';
 	//private $filename = "input/bookmarks.json";	
 	
-	public function __construct()
+	public function __construct($keys)
 	{
 		$this->guzzle = new Client(['timeout'  => $this->timeout]);		
-		$this->keys = parse_ini_file($this->keyfile);
+		$this->keys = $keys;
 	}
 	
 	public function saveBookmark($bookmark)
