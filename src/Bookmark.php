7f705bc8995d72117fae452591183538957921e7
fridde <f@hehl.se>
2017-10-11 18:42:02 +0200

    added classes

---
+++ src/Bookmark.php
@@ -0,0 +1,34 @@
+<?php
+
+class Bookmark {
+	
+	private $title;	
+	private $url;
+	
+	public function __construct($title, $url)
+	{
+		$this->setTitle($title);
+		$this->setUrl($url);
+	}
+	
+	
+	public function setTitle($title)
+	{
+		$this->title = $title;
+	}
+	
+	public function getTitle()
+	{
+		return $this->title;
+	}
+	
+	public function setUrl($url)
+	{
+		$this->url = $url;
+	}
+	
+	public function getUrl()
+	{
+		return $this->url;
+	}
+}
---
+++ src/SavedClient.php
@@ -0,0 +1,29 @@
+<?php
+
+use GuzzleHttp\Client;
+
+class SavedClient {
+	
+	private $guzzle;
+	private $timeout = 2.0;
+	private $keys;
+	private $keyfile = 'keys.ini';
+	private $api_url = 'http://devapi.saved.io/bookmarks/';
+	//private $filename = "input/bookmarks.json";	
+	
+	public function __construct()
+	{
+		$this->guzzle = new Client(['timeout'  => $this->timeout]);		
+		$this->keys = parse_ini_file($this->keyfile);
+	}
+	
+	public function saveBookmark($bookmark)
+	{
+		$query = $this->keys;
+		$query["title"] = $bookmark->getTitle();
+        $query["url"] = $bookmark->getUrl();		        
+        $response = $this->guzzle->request('POST', $this->api_url, ['query' => $query);
+		sleep(0.1);
+	}
+	
+}
